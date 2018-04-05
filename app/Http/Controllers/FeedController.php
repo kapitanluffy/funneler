4eac3efd5ed2018f7532e41ba7f6be0d1ce4b039
Kapitanluffy Pirata <kapitanluffy@gmail.com>
2018-04-05 16:53:07 +0800

    Update FeedController

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -5,19 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\FeedManager\FeedManagerInterface;
 use Illuminate\Routing\ResponseFactory;
-use App\QueueManager\QueueManagerInterface;
 use App\FeedManager\FeedItem;
+use App\Jobs;
 
 class FeedController extends Controller
 {
     public function __construct(
         FeedManagerInterface $feedManager,
-        QueueManagerInterface $queue,
         Request $request,
         ResponseFactory $response
     ) {
         $this->feedManager = $feedManager;
-        $this->queue = $queue;
         $this->request = $request;
         $this->response = $response;
     }
@@ -26,6 +24,7 @@ class FeedController extends Controller
     {
         // $feed = $this->feedManager->load("http://feeds.dzone.com/home");
         $feed = $this->feedManager->load("http://www.reddit.com/r/programming/.rss");
+        // $feed = $this->feedManager->load("https://www.theregister.co.uk/science/geeks_guide/headlines.atom");
 
         return $this->response->json($feed->toArray());
     }
@@ -33,12 +32,12 @@ class FeedController extends Controller
     public function queue()
     {
         $link = $this->request->get('link');
-        $title = $this->request->get('title');
+        $message = $this->request->get('message');
 
-        $item = new FeedItem(['link' => $link, 'title' => $title]);
+        $job = new Jobs\PostToFacebook($message, $link);
 
-        $queuedItem = $this->queue->queue($item);
+        dispatch($job);
 
-        return $this->response->json($queuedItem);
+        return $this->response->json(['message' => 'Item queued']);
     }
 }
